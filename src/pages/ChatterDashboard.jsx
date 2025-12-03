b9acc8de04b5624b0a2a3c3d063f27f63e5cef7a
fl0w <raja.mywebsite123@gmail.com>
2025-12-03 12:59:46 +0000

    File changes

--- src/pages/ChatterDashboard.jsx
+++ src/pages/ChatterDashboard.jsx
@@ -282,23 +282,3 @@ export default function ChatterDashboard() {
     </div>
   );
 }
-
-function GraduationCap(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
-      <path d="M6 12v5c0 2 2 3 6 3s6-1 6-3v-5" />
-    </svg>
-  );
-}
